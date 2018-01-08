7d6cf075666dff38e9c49eb7d0d64abfded533ac
agentleman <675740051@qq.com>
2018-01-08 17:18:43 +0800

    addhome

---
+++ home/service/homeService.js
@@ -0,0 +1,20 @@
+/**
+ *  震川openid：HomeService接口封装
+ *  -----------------------------
+ *  探索与发现：只有当感性的认知不断地被现实摧毁后，才能促使人们进行更深入全面得思考；
+ **************************************/
+angular.module("zcApp").factory("GetOpenIdService", ["GetOpenIdServiceImpl",
+    // function (GetOpenIdServiceImpl) {
+    //     /**
+    //      * 获取openid
+    //      * @param code
+    //      */
+    //     var getOpenId = function(code) {
+    //         return GetOpenIdServiceImpl.getOpenId(code);
+    //     };
+    //
+    //     return {
+    //         getOpenId: getOpenId
+    //     };
+    // }
+      ]);
